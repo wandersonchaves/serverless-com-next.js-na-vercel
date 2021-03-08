b532a5c2824e73438e989284e9b20caf435a260e
Wanderson Chaves <wandersonscpibr@gmail.com>
2021-03-08 16:15:21 -0300

    connect to database and add cachedDb

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,7 +1,36 @@
 import { NowRequest, NowResponse } from "@vercel/node";
+import { MongoClient, Db } from "mongodb";
+import url from "url";
 
-export default (request: NowRequest, response: NowResponse) => {
+let cachedDb: Db = null;
+
+async function connectToDatabase(uri: string) {
+  if (cachedDb) {
+    return cachedDb;
+  }
+
+  const client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+  const dbName = url.parse(uri).pathname.substr(1);
+
+  const db = client.db(dbName);
+
+  cachedDb = db;
+
+  return db;
+}
+
+export default async (request: NowRequest, response: NowResponse) => {
   const { email } = request.body;
 
-  return response.json({ message: { email } });
+  const db = await connectToDatabase(process.env.MONGODB_URI);
+
+  const collection = db.collection("subscribers");
+
+  await collection.insertOne({ email, subscribeAt: new Date() });
+
+  return response.status(201).json({ ok: true });
 };
